0e6b3fbfc28c797a016216efe736b06f56e13d06
Hongyuan Zhang <zhygwu2016@gmail.com>
2019-03-10 23:06:25 -0700

    First Version

---
+++ src/notes.js
@@ -0,0 +1,6 @@
+/*
+1. create-react-app burger_builder
+2. npm run eject
+   https://www.udemy.com/react-the-complete-guide-incl-redux/learn/v4/questions/6085314
+3. npm install --save prop-types
+*/
